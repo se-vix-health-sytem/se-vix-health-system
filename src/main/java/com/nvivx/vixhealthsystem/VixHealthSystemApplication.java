74b45237172119e5cabc8503a68822e58f73b236
Harti Alexandrina <hartialexandrina@gmail.com>
2026-05-26 18:13:12 +0200

    Initial commit

---
+++ src/main/java/com/nvivx/vixhealthsystem/VixHealthSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nvivx.vixhealthsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VixHealthSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VixHealthSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nvivx/vixhealthsystem/VixHealthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvivx.vixhealthsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VixHealthSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
