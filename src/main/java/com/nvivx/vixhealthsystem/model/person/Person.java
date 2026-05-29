ccbc493c91895f66fa43c07761632f765eab83a7
navjotkaur-lab <navjot.kaur@studenti.unitn.it>
2026-05-29 16:20:19 +0200

    "implemented class Person and Employee"

--- src/main/java/com/nvivx/vixhealthsystem/model/person/Person.java
+++ src/main/java/com/nvivx/vixhealthsystem/model/person/Person.java
@@ -1,4 +1,73 @@
 package com.nvivx.vixhealthsystem.model.person;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Person {
+    private String name;
+    private String surname;
+    private LocalDate dateOfBirth;
+    private String placeOfBirth;
+    private char gender;
+    private String email;
+    private String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPlaceOfBirth() {
+        return placeOfBirth;
+    }
+
+    public void setPlaceOfBirth(String placeOfBirth) {
+        this.placeOfBirth = placeOfBirth;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public int getAge() {
+        return Period.between(dateOfBirth,LocalDate.now()).getYears();
+    }
+
 }
--- src/main/java/com/nvivx/vixhealthsystem/model/person/employee/Employee.java
+++ src/main/java/com/nvivx/vixhealthsystem/model/person/employee/Employee.java
@@ -1,4 +1,34 @@
 package com.nvivx.vixhealthsystem.model.person.employee;
 
-public class Employee {
+import com.nvivx.vixhealthsystem.model.person.Person;
+import com.nvivx.vixhealthsystem.model.resource.Resource;
+
+import java.time.LocalDate;
+
+public class Employee extends Person {
+    private int id;
+    private LocalDate hireDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(LocalDate hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    protected Resource[] viewResources() {
+        return null;
+    }
+    protected void takeResource(Resource r){
+
+    }
 }
