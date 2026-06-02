03e1d27b3c95a2079cd32e5dff5261f63f3987c3
vivis2kuni <viviana.fraccaroli@studenti.unitn.it>
2026-06-02 14:51:45 +0200

    Some classes implemented

--- src/main/java/com/nvivx/vixhealthsystem/model/facility/Location.java
+++ src/main/java/com/nvivx/vixhealthsystem/model/facility/Location.java
@@ -1,4 +1,11 @@
 package com.nvivx.vixhealthsystem.model.facility;
 
 public class Location {
+    double latitude;
+    double longitude;
+
+    public Location(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 }
--- src/main/java/com/nvivx/vixhealthsystem/model/resource/Storage.java
+++ src/main/java/com/nvivx/vixhealthsystem/model/resource/Storage.java
@@ -1,4 +1,48 @@
 package com.nvivx.vixhealthsystem.model.resource;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Storage {
+    private int totalQuantity;
+    private Map<Resource, Integer> resources;
+
+    public Storage() {
+        totalQuantity = 0;
+        resources = new HashMap<>();
+    }
+
+    public int getTotalQuantity() {
+        return totalQuantity;
+    }
+
+    private void updateQuantity(int q) {
+        totalQuantity += q;
+    }
+
+    public void addResource(Resource r, int q) {
+        if (resources.containsKey(r)) {
+            int current = resources.get(r);
+            resources.put(r, current + q);
+        } else {
+            resources.put(r, q);
+        }
+
+        updateQuantity(q);
+    }
+
+    public void removeResource(Resource r, int q) throws Exception {
+        if (resources.containsKey(r)) {
+            int current = resources.get(r);
+            resources.put(r, current - q);
+        } else {
+            throw new Exception("Resource not present");
+        }
+
+        updateQuantity(q);
+    }
+
+    public Map<Resource, Integer> getResources() {
+        return resources;
+    }
 }
